caefb047e2b6d7a5530af176a0f6a1ce5984bb56
skytoursdevel1 <vitaly@sky-tours.com>
2025-12-23 05:34:23 +0000

    fixed

--- include/core/airobotCore.class.php
+++ include/core/airobotCore.class.php
@@ -8,7 +8,7 @@ class airobotCore
         if (!Config::get('suppliers')[api::$query->query['provider']]['active']) output::view('STATUS_0006',true);
         $cls = api::$method->name.'_'.api::$method->action;
         $err = $cls::exec() ?? false;
-        Logger::write_buffer('ancillary',api::$query->query['provider']);
+        Logger::write_buffer('ancillary',api::$query->query['provider'].((api::$method->action === 'notification') ? '-notification' : ''));
         output::view(self::$result,$err);
     }
 }
--- include/core/gordianCore.class.php
+++ include/core/gordianCore.class.php
@@ -7,7 +7,7 @@ class gordianCore
     {
         if (!Config::get('suppliers')[api::$query->query['provider']]['active']) output::view('STATUS_0006',true);
         $err = api::$method->name::exec() ?? false;
-        Logger::write_buffer('ancillary',api::$query->query['provider']);
+        Logger::write_buffer('ancillary',api::$query->query['provider'].((api::$method->name === 'notification') ? '-notification' : ''));
         output::view(self::$result,$err);
     }
 }
